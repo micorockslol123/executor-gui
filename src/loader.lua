4a42d94234a84d0652a1c6593e22893c39fa0360
zenoiscool <98782020+micorockslol123@users.noreply.github.com>
2024-12-18 14:48:39 -0800

    Update loader.lua

--- src/loader.lua
+++ src/loader.lua
@@ -1,6 +1,6 @@
 -- config
 local repoName = "executor-gui"
-local repoOwner = "null"
+local repoOwner = "micorockslol123"
 -- variables
 local http_request = (syn and syn.request) or (http and http.request) or request or http_request
 local wrapperEnv = {}
